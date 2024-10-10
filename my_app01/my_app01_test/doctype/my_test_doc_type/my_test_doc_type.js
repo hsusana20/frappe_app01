d349af26c884703b6e5a3b4e381df3bc8172df43
hsusana20 <152581281+hsusana20@users.noreply.github.com>
2024-10-10 06:42:49 +0000

    Create New Doc type

---
+++ my_app01/my_app01_test/doctype/my_test_doc_type/my_test_doc_type.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Heiro Usana and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("My Test Doc Type", {
+// 	refresh(frm) {
+
+// 	},
+// });
